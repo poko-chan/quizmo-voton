d66623653a49b285b733d144a96288d9594b7c5c
pokochan <kohki.hidai@gmail.com>
2026-05-28 17:29:26 +0900

    3

--- resources/js/join/index_script.js
+++ resources/js/join/index_script.js
@@ -1,4 +1,4 @@
 function quizcode() {
     let codes = document.getElementById("code").value;
-    window.location.href =  `https://quizmo-voton.vercel.app/quiz/${codes}/index.html`;
+    window.location.href =  `/quiz/${codes}/`;
 }
